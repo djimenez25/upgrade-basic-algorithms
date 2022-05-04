aca76624764a2c7042079fa1c82c799ad2ab792d
djimenez25 <daniel.jimenez@bootcamp-upgrade.com>
2022-05-04 13:22:37 +0200

    Iteracion#3

---
+++ operadores.js
@@ -0,0 +1,38 @@
+// 1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+// 1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+// 1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+// 1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+// teniendo dos variables y = 10 y z = 5.
+
+// 1.5 Usa el correcto operador de asignación que resultará en x = 50,
+// teniendo dos variables y = 10 y z = 5.
+
+console.log ('Archivo Operadores.js funcionando OK')
+
+// 1.1
+let num = 10;
+let num1 = 5;
+alert (num * num1);
+
+// 1.2
+let num2 = 2;
+alert (num / num2)
+
+// 1.3
+const num3 = 15;
+const num4 = 9;
+alert (num3 % num4)
+
+
+// 1.4
+let y = 10;
+let z = 5;
+let x = y + z;
+console.log (x)
+
+// 1.5
+let x1 = y * z;
+console.log(x1)
